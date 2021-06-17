e4db6e96af157e8db55a9a2dbd09ec1880778443
Michael Trpak <mrtrpak3@gmail.com>
2021-06-17 15:27:42 -0400

    adding message

--- react-key-concepts/src/ClassComponent.js
+++ react-key-concepts/src/ClassComponent.js
@@ -9,7 +9,7 @@ class ClassComponent extends Component {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hello There World
         </p>
         <a
           className="App-link"
